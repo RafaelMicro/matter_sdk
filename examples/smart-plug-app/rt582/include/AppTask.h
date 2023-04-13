7f4240b6cb24c9588f938f3c2743bf5342724134
Rex <rex.huang@rafaelmicro.com>
2023-04-13 19:07:35 +0800

    Add example : smart plug

---
+++ examples/smart-plug-app/rt582/include/AppTask.h
@@ -0,0 +1,120 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2019 Google LLC.
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+/**********************************************************
+ * Includes
+ *********************************************************/
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "AppEvent.h"
+#include "BaseApplication.h"
+#include "FreeRTOS.h"
+#include "SmartPlugManager.h"
+// #include "sl_simple_button_instances.h"
+#include "timers.h" // provides FreeRTOS timer support
+#include <app/clusters/identify-server/identify-server.h>
+#include <ble/BLEEndPoint.h>
+#include <lib/core/CHIPError.h>
+#include <platform/CHIPDeviceLayer.h>
+
+#include "bsp.h"
+
+/**********************************************************
+ * Defines
+ *********************************************************/
+
+// Application-defined error codes in the CHIP_ERROR space.
+#define APP_ERROR_EVENT_QUEUE_FAILED CHIP_APPLICATION_ERROR(0x01)
+#define APP_ERROR_CREATE_TASK_FAILED CHIP_APPLICATION_ERROR(0x02)
+#define APP_ERROR_UNHANDLED_EVENT CHIP_APPLICATION_ERROR(0x03)
+#define APP_ERROR_CREATE_TIMER_FAILED CHIP_APPLICATION_ERROR(0x04)
+#define APP_ERROR_START_TIMER_FAILED CHIP_APPLICATION_ERROR(0x05)
+#define APP_ERROR_STOP_TIMER_FAILED CHIP_APPLICATION_ERROR(0x06)
+
+/**********************************************************
+ * AppTask Declaration
+ *********************************************************/
+
+class AppTask
+{
+
+public:
+    CHIP_ERROR StartAppTask();
+    static void AppTaskMain(void * pvParameter);
+
+    void PostLightActionRequest(int32_t aActor, SmartPlugManager::Action_t aAction);
+    void PostEvent(const AppEvent * event);
+    void PostAppIdentify();
+
+    static void IdentifyStartHandler(Identify *);
+    static void IdentifyStopHandler(Identify *);
+    static void IdentifyHandleOp(AppEvent * aEvent);
+private:
+    friend AppTask & GetAppTask(void);
+
+    CHIP_ERROR Init();
+    static void InitServer(intptr_t arg);
+    static void OpenCommissioning(intptr_t arg);
+    static void ChipEventHandler(const chip::DeviceLayer::ChipDeviceEvent *, intptr_t);
+    static void ActionEventHandler(AppEvent * aEvent);
+    static void ActionInitiated(SmartPlugManager::Action_t aAction);
+    static void ActionCompleted(SmartPlugManager::Action_t aAction);
+    static void UpdateStatusLED();
+    static void UpdateClusterState(intptr_t arg);
+    void DispatchEvent(AppEvent * event);
+
+    static void ButtonEventHandler(bsp_event_t event);
+
+    static void FunctionTimerEventHandler(AppEvent * aEvent);
+    static void FunctionHandler(AppEvent * aEvent);
+
+    static void TimerEventHandler(chip::System::Layer * aLayer, void * aAppState);
+
+    void StartTimer(uint32_t aTimeoutMs);
+    void CancelTimer(void);
+
+    enum Function_t
+    {
+        kFunction_NoneSelected   = 0,
+        kFunction_FactoryReset   = 1,
+        kFunction_Switch_1       = 2,
+
+        kFunction_Invalid
+    } Function;
+
+    Function_t mFunction;
+    bool mFunctionTimerActive;
+    bool mFunctionSwitchActive;
+    bool mSyncClusterToButtonAction;
+
+    static AppTask sAppTask;   
+
+#if CONFIG_CHIP_FACTORY_DATA
+    chip::DeviceLayer::FactoryDataProvider<chip::DeviceLayer::InternalFlashFactoryData> mFactoryDataProvider;
+#endif
+
+};
+inline AppTask & GetAppTask(void)
+{
+    return AppTask::sAppTask;
+}
---
+++ examples/smart-plug-app/rt582/src/SmartPlugManager.cpp
@@ -0,0 +1,120 @@
+/*
+ *
+ *    Copyright (c) 2020 Project CHIP Authors
+ *    Copyright (c) 2019 Google LLC.
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "SmartPlugManager.h"
+
+#include "AppConfig.h"
+#include "AppTask.h"
+#include <FreeRTOS.h>
+#include <app/clusters/on-off-server/on-off-server.h>
+
+#include <app-common/zap-generated/af-structs.h>
+#include <app-common/zap-generated/attributes/Accessors.h>
+
+
+using namespace chip;
+using namespace chip::DeviceLayer;
+
+SmartPlugManager SmartPlugManager::sPlug;
+
+CHIP_ERROR SmartPlugManager::Init()
+{
+    bool currentLedState;
+    // read current on/off value on endpoint one.
+
+    OnOffServer::Instance().getOnOffValue(1, &currentLedState);
+
+    mState = currentLedState ? kState_On : kState_Off;
+
+    return CHIP_NO_ERROR;
+}
+
+void SmartPlugManager::SetCallbacks(SmartPlugCallback_fn aActionInitiated_CB, SmartPlugCallback_fn aActionCompleted_CB)
+{
+    mActionInitiated_CB = aActionInitiated_CB;
+    mActionCompleted_CB = aActionCompleted_CB;
+}
+
+bool SmartPlugManager::IsTurnedOn()
+{
+    return mState == kState_On;
+}
+
+
+bool SmartPlugManager::InitiateAction(Action_t aAction, int32_t aActor, uint16_t size, uint8_t * value)
+{
+    bool action_initiated = false;
+    State_t new_state;
+    switch (aAction)
+    {
+    case ON_ACTION:
+        ChipLogProgress(NotSpecified, "PlugMgr:ON: %s->ON", mState == kState_On ? "ON" : "OFF");
+        break;
+    case OFF_ACTION:
+        ChipLogProgress(NotSpecified, "PlugMgr:OFF: %s->OFF", mState == kState_On ? "ON" : "OFF");
+        break;    
+    default:
+        ChipLogProgress(NotSpecified, "PlugMgr:Unknown");
+        break;
+    }
+
+    // Initiate Turn On/Off Action only when the previous one is complete.
+    if (mState == kState_Off && aAction == ON_ACTION)
+    {
+        action_initiated = true;
+
+        mState = kState_On;
+    }
+    else if (mState == kState_On && aAction == OFF_ACTION)
+    {
+        action_initiated = true;
+
+        mState = kState_Off;
+    }
+    if (action_initiated)
+    {        
+        if (mActionInitiated_CB)
+        {
+            mActionInitiated_CB(aAction);
+        }
+        else
+        {
+            Set(mState == kState_On);
+        }
+
+        if (mActionCompleted_CB)
+        {
+            mActionCompleted_CB(aAction);
+        }
+    }
+
+    return action_initiated;
+}
+
+void SmartPlugManager::Set(bool aOn)
+{
+    if (aOn)
+    {
+        mState = kState_On;
+    }
+    else
+    {
+        mState = kState_Off;
+    }
+}

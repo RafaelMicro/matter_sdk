36a8883dc5da7a755ba2b3ac60f9ebf7304d20d3
Rex <rex.huang@rafaelmicro.com>
2023-05-11 15:49:59 +0800

    Fix factoryreset issue

--- third_party/rafael/sdk/Driver/Peripheral/flashctl.c
+++ third_party/rafael/sdk/Driver/Peripheral/flashctl.c
@@ -6,8 +6,8 @@
                                                                               * @copyright
                                                                               *****************************************************************************/
 
-#include "project_config.h"
 #include "cm3_mcu.h"
+#include "project_config.h"
 
 #define FLASH_UNLOCK_PATTER 0x52414254
 
@@ -21,22 +21,22 @@ void flash_suspend_check(void)
 
     if ((flash_status.status2 & FLASH_SUSPEND_STATUS) == FLASH_SUSPEND_STATUS) // suspend status
     {
-        FLASH->COMMAND = CMD_MANUAL_MODE;            // manual mode
-        FLASH->FLASH_INSTR = CMD_FLASH_RESET_ENABLE; // enable reset
-        FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-        FLASH->START = STARTBIT;
+        FLASH->COMMAND     = CMD_MANUAL_MODE;                                  // manual mode
+        FLASH->FLASH_INSTR = CMD_FLASH_RESET_ENABLE;                           // enable reset
+        FLASH->PATTERN     = FLASH_UNLOCK_PATTER;
+        FLASH->START       = STARTBIT;
         while (flash_check_busy())
         {
             ;
-        } // wait busy finish
+        }                                     // wait busy finish
 
         FLASH->FLASH_INSTR = CMD_FLASH_RESET; // reset
-        FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-        FLASH->START = STARTBIT;
+        FLASH->PATTERN     = FLASH_UNLOCK_PATTER;
+        FLASH->START       = STARTBIT;
         while (flash_check_busy())
         {
             ;
-        } // wait busy finish
+        }             // wait busy finish
 
         Delay_ms(25); // Delay time
     }
@@ -99,6 +99,11 @@ uint32_t flash_check_busy()
  */
 uint32_t flash_read_page(uint32_t buf_addr, uint32_t read_page_addr)
 {
+
+#if (RAFAEL_BOARD == RT582)
+    if (read_page_addr >= 0x100000)
+        read_page_addr ^= 0x100000;
+#endif
     if (flash_check_address(read_page_addr, LENGTH_PAGE) == STATUS_INVALID_PARAM)
     {
         return STATUS_INVALID_PARAM; // invalid addres range
@@ -111,11 +116,11 @@ uint32_t flash_read_page(uint32_t buf_addr, uint32_t read_page_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READPAGE;
+    FLASH->COMMAND    = CMD_READPAGE;
     FLASH->FLASH_ADDR = read_page_addr;
-    FLASH->MEM_ADDR = buf_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->MEM_ADDR   = buf_addr;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -130,6 +135,11 @@ uint32_t flash_read_page(uint32_t buf_addr, uint32_t read_page_addr)
  */
 uint32_t flash_read_page_syncmode(uint32_t buf_addr, uint32_t read_page_addr)
 {
+#if (RAFAEL_BOARD == RT582)
+    if (read_page_addr >= 0x100000)
+        read_page_addr ^= 0x100000;
+#endif
+
     if (flash_check_address(read_page_addr, LENGTH_PAGE) == STATUS_INVALID_PARAM)
     {
         return STATUS_INVALID_PARAM; // invalid addres range
@@ -142,11 +152,11 @@ uint32_t flash_read_page_syncmode(uint32_t buf_addr, uint32_t read_page_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READPAGE;
+    FLASH->COMMAND    = CMD_READPAGE;
     FLASH->FLASH_ADDR = read_page_addr;
-    FLASH->MEM_ADDR = buf_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->MEM_ADDR   = buf_addr;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -160,6 +170,11 @@ uint32_t flash_read_page_syncmode(uint32_t buf_addr, uint32_t read_page_addr)
 
 uint8_t flash_read_byte(uint32_t read_addr)
 {
+#if (RAFAEL_BOARD == RT582)
+    if (read_addr >= 0x100000)
+        read_addr ^= 0x100000;
+#endif
+
     /*this is not a good idea to block function here....*/
     while (flash_check_busy())
     {
@@ -168,10 +183,10 @@ uint8_t flash_read_byte(uint32_t read_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READBYTE;
+    FLASH->COMMAND    = CMD_READBYTE;
     FLASH->FLASH_ADDR = read_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -183,8 +198,13 @@ uint8_t flash_read_byte(uint32_t read_addr)
     return FLASH->FLASH_DATA >> 8;
 }
 
-uint32_t flash_read_byte_check_addr(uint32_t *buf_addr, uint32_t read_addr)
+uint32_t flash_read_byte_check_addr(uint32_t * buf_addr, uint32_t read_addr)
 {
+#if (RAFAEL_BOARD == RT582)
+    if (read_addr >= 0x100000)
+        read_addr ^= 0x100000;
+#endif
+
     if (flash_check_address(read_addr, LENGTH_BYTE) == STATUS_INVALID_PARAM)
     {
         return STATUS_INVALID_PARAM; // invalid addres range
@@ -197,10 +217,10 @@ uint32_t flash_read_byte_check_addr(uint32_t *buf_addr, uint32_t read_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READBYTE;
+    FLASH->COMMAND    = CMD_READBYTE;
     FLASH->FLASH_ADDR = read_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -221,12 +241,15 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
         return STATUS_INVALID_PARAM;
     }
 
+#if (RAFAEL_BOARD == RT582)
+    if (flash_addr >= 0x100000)
+        flash_addr ^= 0x100000;
+#endif
     /* For Safety reason, we don't implement
      * erase chip command here. */
     switch (mode)
     {
-    case FLASH_ERASE_PAGE:
-    {
+    case FLASH_ERASE_PAGE: {
         if ((flash_get_deviceinfo() & 0xFF) != PUYA_MANU_ID)
         {
             return STATUS_INVALID_PARAM; // invalid flash id
@@ -240,8 +263,7 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
         FLASH->COMMAND = CMD_ERASEPAGE;
         break;
     }
-    case FLASH_ERASE_SECTOR:
-    {
+    case FLASH_ERASE_SECTOR: {
         if (flash_check_address(flash_addr, LENGTH_4KB) == STATUS_INVALID_PARAM)
         {
             return STATUS_INVALID_PARAM; // invalid addres range
@@ -250,8 +272,7 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
         FLASH->COMMAND = CMD_ERASESECTOR;
         break;
     }
-    case FLASH_ERASE_32K:
-    {
+    case FLASH_ERASE_32K: {
         if (flash_check_address(flash_addr, LENGTH_32KB) == STATUS_INVALID_PARAM)
         {
             return STATUS_INVALID_PARAM; // invalid addres range
@@ -260,8 +281,7 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
         FLASH->COMMAND = CMD_ERASE_BL32K;
         break;
     }
-    case FLASH_ERASE_64K:
-    {
+    case FLASH_ERASE_64K: {
         if (flash_check_address(flash_addr, LENGTH_64KB) == STATUS_INVALID_PARAM)
         {
             return STATUS_INVALID_PARAM; // invalid addres range
@@ -270,8 +290,7 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
         FLASH->COMMAND = CMD_ERASE_BL64K;
         break;
     }
-    case FLASH_ERASE_SECURE:
-    {
+    case FLASH_ERASE_SECURE: {
         /*This is special command for erase secure register*/
         FLASH->COMMAND = CMD_ERASE_SEC_PAGE;
         break;
@@ -289,8 +308,8 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
     enter_critical_section();
 
     FLASH->FLASH_ADDR = flash_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     while (flash_check_busy())
     {
@@ -302,17 +321,22 @@ uint32_t flash_erase(flash_erase_mode_t mode, uint32_t flash_addr)
     return STATUS_SUCCESS;
 }
 
-void flash_set_timing(flash_timing_mode_t *timing_cfg)
+void flash_set_timing(flash_timing_mode_t * timing_cfg)
 {
-    FLASH->DPD = timing_cfg->deep_pd_timing;
-    FLASH->RDPD = timing_cfg->deep_rpd_timing;
+    FLASH->DPD     = timing_cfg->deep_pd_timing;
+    FLASH->RDPD    = timing_cfg->deep_rpd_timing;
     FLASH->SUSPEND = timing_cfg->suspend_timing;
-    FLASH->RESUME = timing_cfg->resume_timing;
+    FLASH->RESUME  = timing_cfg->resume_timing;
     return;
 }
 
 uint32_t flash_write_page(uint32_t buf_addr, uint32_t write_page_addr)
 {
+#if (RAFAEL_BOARD == RT582)
+    if (write_page_addr >= 0x100000)
+        write_page_addr ^= 0x100000;
+#endif
+
     if (flash_check_address(write_page_addr, LENGTH_PAGE) == STATUS_INVALID_PARAM)
     {
         return STATUS_INVALID_PARAM; // invalid addres range
@@ -326,11 +350,11 @@ uint32_t flash_write_page(uint32_t buf_addr, uint32_t write_page_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_WRITEPAGE;
+    FLASH->COMMAND    = CMD_WRITEPAGE;
     FLASH->FLASH_ADDR = write_page_addr;
-    FLASH->MEM_ADDR = buf_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->MEM_ADDR   = buf_addr;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     while (flash_check_busy())
     {
@@ -344,6 +368,11 @@ uint32_t flash_write_page(uint32_t buf_addr, uint32_t write_page_addr)
 
 uint32_t flash_verify_page(uint32_t read_page_addr)
 {
+#if (RAFAEL_BOARD == RT582)
+    if (read_page_addr >= 0x100000)
+        read_page_addr ^= 0x100000;
+#endif
+
     if (flash_check_address(read_page_addr, LENGTH_PAGE) == STATUS_INVALID_PARAM)
     {
         return STATUS_INVALID_PARAM; // invalid addres range
@@ -357,10 +386,10 @@ uint32_t flash_verify_page(uint32_t read_page_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READVERIFY;
+    FLASH->COMMAND    = CMD_READVERIFY;
     FLASH->FLASH_ADDR = read_page_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -369,6 +398,11 @@ uint32_t flash_verify_page(uint32_t read_page_addr)
 
 uint32_t flash_write_byte(uint32_t write_addr, uint8_t singlebyte)
 {
+#if (RAFAEL_BOARD == RT582)
+    if (write_addr >= 0x100000)
+        write_addr ^= 0x100000;
+#endif
+
     if (flash_check_address(write_addr, LENGTH_BYTE) == STATUS_INVALID_PARAM)
     {
         return STATUS_INVALID_PARAM; // invalid addres range
@@ -382,11 +416,11 @@ uint32_t flash_write_byte(uint32_t write_addr, uint8_t singlebyte)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_WRITEBYTE;
+    FLASH->COMMAND    = CMD_WRITEBYTE;
     FLASH->FLASH_ADDR = write_addr;
     FLASH->FLASH_DATA = singlebyte;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -394,7 +428,7 @@ uint32_t flash_write_byte(uint32_t write_addr, uint8_t singlebyte)
 }
 
 /*get Flash status register*/
-uint32_t flash_get_status_reg(flash_status_t *status)
+uint32_t flash_get_status_reg(flash_status_t * status)
 {
     /*2022/04/28 add, Device busy. try again.*/
     if (flash_check_busy())
@@ -408,7 +442,7 @@ uint32_t flash_get_status_reg(flash_status_t *status)
 
         FLASH->COMMAND = CMD_READ_STATUS1;
         FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-        FLASH->START = STARTBIT;
+        FLASH->START   = STARTBIT;
 
         leave_critical_section();
 
@@ -417,7 +451,7 @@ uint32_t flash_get_status_reg(flash_status_t *status)
         {
             ;
         }
-        status->status1 = (uint8_t)((FLASH->FLASH_DATA) >> 8);
+        status->status1 = (uint8_t) ((FLASH->FLASH_DATA) >> 8);
     }
 
     if (status->require_mode & FLASH_STATUS_RW2)
@@ -426,7 +460,7 @@ uint32_t flash_get_status_reg(flash_status_t *status)
 
         FLASH->COMMAND = CMD_READ_STATUS2;
         FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-        FLASH->START = STARTBIT;
+        FLASH->START   = STARTBIT;
 
         leave_critical_section();
 
@@ -434,7 +468,7 @@ uint32_t flash_get_status_reg(flash_status_t *status)
         {
             ;
         }
-        status->status2 = (uint8_t)((FLASH->FLASH_DATA) >> 8);
+        status->status2 = (uint8_t) ((FLASH->FLASH_DATA) >> 8);
     }
 
     /*2022/01/18: GD does NOT have status bytes3.*/
@@ -443,7 +477,7 @@ uint32_t flash_get_status_reg(flash_status_t *status)
 }
 
 /*set Flash status register*/
-uint32_t flash_set_status_reg(const flash_status_t *status)
+uint32_t flash_set_status_reg(const flash_status_t * status)
 {
 
     /*2022/04/28 add, Device busy. try again.*/
@@ -461,9 +495,9 @@ uint32_t flash_set_status_reg(const flash_status_t *status)
         enter_critical_section();
         /*GD write status2 must two bytes */
         FLASH->COMMAND = CMD_WRITE_STATUS;
-        FLASH->STATUS = (uint32_t)(status->status1) | (uint32_t)((status->status2) << 8);
+        FLASH->STATUS  = (uint32_t) (status->status1) | (uint32_t) ((status->status2) << 8);
         FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-        FLASH->START = STARTBIT;
+        FLASH->START   = STARTBIT;
 
         leave_critical_section();
 
@@ -477,9 +511,9 @@ uint32_t flash_set_status_reg(const flash_status_t *status)
         enter_critical_section();
 
         FLASH->COMMAND = CMD_WRITE_STATUS1;
-        FLASH->STATUS = (status->status1);
+        FLASH->STATUS  = (status->status1);
         FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-        FLASH->START = STARTBIT;
+        FLASH->START   = STARTBIT;
 
         leave_critical_section();
 
@@ -516,11 +550,11 @@ uint32_t flash_write_sec_register(uint32_t buf_addr, uint32_t write_reg_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_WRITE_SEC_PAGE;
+    FLASH->COMMAND    = CMD_WRITE_SEC_PAGE;
     FLASH->FLASH_ADDR = write_reg_addr;
-    FLASH->MEM_ADDR = buf_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->MEM_ADDR   = buf_addr;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -551,11 +585,11 @@ uint32_t flash_read_sec_register(uint32_t buf_addr, uint32_t read_reg_addr)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READ_SEC_PAGE;
+    FLASH->COMMAND    = CMD_READ_SEC_PAGE;
     FLASH->FLASH_ADDR = read_reg_addr;
-    FLASH->MEM_ADDR = buf_addr;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->MEM_ADDR   = buf_addr;
+    FLASH->PATTERN    = FLASH_UNLOCK_PATTER;
+    FLASH->START      = STARTBIT;
 
     leave_critical_section();
 
@@ -599,15 +633,15 @@ uint32_t flash_get_unique_id(uint32_t flash_id_buf_addr, uint32_t buf_length)
 
     enter_critical_section();
 
-    FLASH->COMMAND = CMD_READUID;
+    FLASH->COMMAND        = CMD_READUID;
     FLASH->PAGE_READ_WORD = 0xF;
-    FLASH->MEM_ADDR = (uint32_t)temp;
-    FLASH->PATTERN = FLASH_UNLOCK_PATTER;
-    FLASH->START = STARTBIT;
+    FLASH->MEM_ADDR       = (uint32_t) temp;
+    FLASH->PATTERN        = FLASH_UNLOCK_PATTER;
+    FLASH->START          = STARTBIT;
 
     leave_critical_section();
 
-    ptr = (uint8_t *)flash_id_buf_addr; /*set address*/
+    ptr = (uint8_t *) flash_id_buf_addr; /*set address*/
 
     while (flash_check_busy())
     {
@@ -639,36 +673,36 @@ void flash_timing_init(void)
     /*check flash type to adjust flash timing*/
     if (flash_type_id == GDWQ_ID)
     {
-        tdp = GDWQ_FLASH_TDP;
+        tdp  = GDWQ_FLASH_TDP;
         tres = GDWQ_FLASH_TRES1;
         tsus = GDWQ_FLASH_TSUS;
-        trs = GDWQ_FLASH_TRS;
+        trs  = GDWQ_FLASH_TRS;
     }
 
     if (flash_type_id == GDLQ_ID)
     {
-        tdp = GDLQ_FLASH_TDP;
+        tdp  = GDLQ_FLASH_TDP;
         tres = GDLQ_FLASH_TRES1;
         tsus = GDLQ_FLASH_TSUS;
-        trs = GDLQ_FLASH_TRS;
+        trs  = GDLQ_FLASH_TRS;
     }
 
     if (flash_type_id == PUYA_ID)
     {
-        tdp = PUYA_FLASH_TDP;
+        tdp  = PUYA_FLASH_TDP;
         tres = PUYA_FLASH_TRES1;
         tsus = PUYA_FLASH_TSUS;
-        trs = PUYA_FLASH_TRS;
+        trs  = PUYA_FLASH_TRS;
     }
     //(0+2)*16=32
     //(1+2)*16=48
     //(2+2)*16=64
     sys_clk = (clk_mode + 2) << 4;
 
-    flash_timing.deep_pd_timing = tdp * sys_clk + 2;
+    flash_timing.deep_pd_timing  = tdp * sys_clk + 2;
     flash_timing.deep_rpd_timing = tres * sys_clk + 2;
-    flash_timing.suspend_timing = tsus * sys_clk + 2;
-    flash_timing.resume_timing = trs * sys_clk + 2;
+    flash_timing.suspend_timing  = tsus * sys_clk + 2;
+    flash_timing.resume_timing   = trs * sys_clk + 2;
 
     flash_set_timing(&flash_timing);
 }
@@ -677,16 +711,16 @@ uint32_t flash_read_otp_sec_page(uint32_t buf_addr)
 {
     switch (flash_get_deviceinfo()) // check flash device
     {
-    case RT581_FLASH_TYPE:     // 0x2000
-    case RT582P512_FLASH_TYPE: // 0x2000
-        if (flash_read_sec_register((uint32_t)buf_addr, FLASH_SECREG_R2_P0))
+    case RT581_FLASH_TYPE:          // 0x2000
+    case RT582P512_FLASH_TYPE:      // 0x2000
+        if (flash_read_sec_register((uint32_t) buf_addr, FLASH_SECREG_R2_P0))
         {
             return STATUS_INVALID_PARAM;
         }
         break;
     case RT582_FLASH_TYPE:    // 0x0000
     case RT582_2M_FLASH_TYPE: // 0x0000
-        if (flash_read_sec_register((uint32_t)buf_addr, FLASH_SECREG_R0_P0))
+        if (flash_read_sec_register((uint32_t) buf_addr, FLASH_SECREG_R0_P0))
         {
             return STATUS_INVALID_PARAM;
         }
